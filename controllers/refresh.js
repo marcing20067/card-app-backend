e8f62fe77b10f4d3df48f99d7581ae8ee98a5b10
Marcin8013 <marcing2067@wp.pl>
2021-08-20 20:34:23 +0200

    Delete unnecessary refreshToken function

--- controllers/refresh.js
+++ controllers/refresh.js
@@ -3,26 +3,6 @@ const config = require('../config/config.js');
 const errorTexts = require('../errorTexts/errorTexts.js');
 const invalidRefreshTokenErrorText = errorTexts.controllers.refresh.invalidRefreshToken;
 
-
-exports.refreshToken = (req, res, next) => {
-    const token = req.cookies.refreshToken;
-
-    try {
-        const userData = jwt.verify(token, config.REFRESH_TOKEN)
-        const newAccessToken = jwt.sign(userData, config.ACCESS_TOKEN);
-        const newRefreshToken = jwt.sign(userData, config.REFRESH_TOKEN);
-
-        res.cookie('refreshToken', newRefreshToken, {
-            maxAge: config.REFRESH_TOKEN_EXPIRES_IN_MILISECONDS,
-            httpOnly: true
-        });
-
-        res.status(201).send({ accessToken: newAccessToken, accessTokenExpiresIn: config.ACCESS_TOKEN_EXPIRES_IN_SECONDS, })
-    } catch {
-        res.status(400).send({ message: invalidRefreshTokenErrorText })
-    }
-}
-
 exports.refresh = (req, res, next) => {
     const token = req.cookies.refreshToken;
 
