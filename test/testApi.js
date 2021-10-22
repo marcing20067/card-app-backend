987981d6d3495c7708c444a56b127e815860eaaa
Marcin8013 <marcing2067@wp.pl>
2021-10-22 18:19:46 +0200

    Delete unnecessary functions from testApi file

--- test/testApi.js
+++ test/testApi.js
@@ -1,6 +1,5 @@
 const httpRequest = require('supertest');
 const User = require('../models/user');
-const OneTimeToken = require('../models/oneTimeToken');
 const Set = require('../models/set');
 const jsonwebtoken = require('jsonwebtoken');
 jest.mock('jsonwebtoken');
@@ -54,24 +53,6 @@ const validSet = {
     creator: 'creator'
 }
 
-const responseStatusShouldBe = (response, status) => {
-    expect(response.status).toBe(status);
-}
-
-const responseTypeShouldContainJson = (response) => {
-    const contentType = response.headers['content-type'];
-    expect(/json/.test(contentType))
-}
-
-const responseBodyShouldContainProperty = (response, property) => {
-    expect(response.body).toHaveProperty(property);
-}
-
-const messageShouldBe = (response, correctMessage) => {
-    const message = response.body.message;
-    expect(message).toBe(correctMessage);
-}
-
 const request = (app, options, lowercaseMethod) => {
     const { method, endpoint, customCookie, data } = options;
     let request = httpRequest(app)[lowercaseMethod](endpoint);
@@ -111,29 +92,6 @@ const makeHttpRequest = async (app, options) => {
     return response;
 }
 
-let token;
-
-const getToken = async (app) => {
-    if (!token) {
-        const response = await makeHttpRequest(app, {
-            method: 'POST',
-            endpoint: '/login',
-            data: validUser
-        });
-        token = response.body.accessToken
-    }
-    return token;
-}
-
-const findOrCreateValidUser = async () => {
-    const findedUser = await findUser(validUser);
-    if (findedUser) {
-        return findedUser;
-    }
-    const createdUser = await createUser({ ...validUser, isActivated: true });
-    return createdUser;
-}
-
 const createValidUser = async () => {
     const userData = generateValidUser();
     const newUser = new User(userData);
@@ -141,76 +99,11 @@ const createValidUser = async () => {
     return createdUser;
 }
 
-const findUser = async (filterData) => {
-    const findedUser = await User.findOne(filterData);
-    return findedUser;
-}
-
-const createUser = async () => {
-    const user = new User({ ...validUser, isActivated: true });
-    await user.save();
-}
-
-const createOneTimeToken = async (customTokenData) => {
-    const randomToken = 'dasud92ddsay9dsa12IYDsuadi' + Math.floor(Math.random() * 500);
-    const oneTimeTokenData = {
-        resetPassword: {
-            token: randomToken,
-            endOfValidity: generateEndOfValidity()
-        },
-        resetUsername: {
-            token: randomToken,
-            endOfValidity: generateEndOfValidity()
-        },
-        activation: {
-            token: randomToken,
-            endOfValidity: generateEndOfValidity()
-        },
-        creator: '',
-        ...customTokenData
-    }
-
-    const newOneTimeToken = new OneTimeToken({ ...oneTimeTokenData });
-    const createdOneTimeToken = await newOneTimeToken.save();
-    return createdOneTimeToken;
-}
-
-const generateEndOfValidity = () => {
-    const oneTimeTokenExpiresInWeeks = 1;
-    const now = new Date();
-    const endOfValidity = new Date().setDate(now.getDate() + oneTimeTokenExpiresInWeeks * 7);
-    return endOfValidity;
-}
-
-const getRandomUserData = () => {
-    const letters = 'qwertyuiopasdfghjklzxcvbnm';
-    const usernameLength = 8;
-    let username = '';
-    for (let i = 0; i < usernameLength; i++) {
-        const randomIndex = Math.floor(Math.random() * letters.length);
-        username = username + letters[randomIndex];
-    }
-    return {
-        ...validUser,
-        username: username,
-        email: 'changePassword@example.com',
-        isActivated: true
-    }
-}
-
 module.exports = {
     newUser,
     validUser,
     validSet,
-    responseStatusShouldBe,
-    responseTypeShouldContainJson,
-    responseBodyShouldContainProperty,
-    messageShouldBe,
     makeHttpRequest,
-    getToken,
-    findOrCreateValidUser,
     createValidUser,
-    createOneTimeToken,
-    getRandomUserData,
     createValidSet
 }
