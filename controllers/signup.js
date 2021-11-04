01ce689bc14abf6a1efa73dfc0eaca3a1f3f3a45
Marcin8013 <marcing2067@wp.pl>
2021-11-04 19:52:13 +0100

    Change hashed password and onetimetoken length

--- controllers/signup.js
+++ controllers/signup.js
@@ -19,10 +19,8 @@ exports.signup = async (req, res, next) => {
         await newUser.validate();
         newUser.password = await bcrypt.hash(newUser.password, config.HASHED_PASSWORD_LENGTH);
         const createdUser = await newUser.save({ validateBeforeSave: false });
-
         const newOneTimeToken = new OneTimeToken({ creator: createdUser._id });
         const createdOneTimeToken = await newOneTimeToken.save();
-
         res.status(201).send({ message: messages.oneTimeToken.newTokenHasBeenCreated });
         createdOneTimeToken.sendEmailWithToken('activation');
     } catch (err) {
