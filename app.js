689ec935339196df6d67161ffec9abdaa8a67ffc
Marcin8013 <marcing2067@wp.pl>
2021-12-13 21:17:35 +0100

    Fix origin in cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({
-    origin: 'http://localhost:4200, https://card-app-backend.netlify.app/auth/login',
+    origin: ['http://localhost:4200', 'https://card-app-backend.netlify.app/auth/login'],
 }))
 
 
