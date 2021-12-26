58e23dcad6295d70d4a669986748d3a686adbb7e
Marcin8013 <marcing2067@wp.pl>
2021-12-27 00:52:13 +0100

    Improve cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://card-app-backend.herokuapp.com'],
 }))
 
 app.get('*', (req, res, next) => {
